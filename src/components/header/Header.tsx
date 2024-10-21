1ee2f92d259154023d2862b1b2391db7df63c67e
mpmora00 <maria.p.mora.bolanos.22@dartmouth.edu>
2024-10-20 21:09:40 -0400

    Reviewing small design comments

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,46 +1,47 @@
 import { useState } from 'react';
-import { Box, Button, styled, MenuItem, Tooltip } from '@mui/material';
+import { Box, Button, MenuItem, Menu } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
 import { PageTab } from '@/information/Routing.tsx'
 import styles from './Header.module.scss';
 import classNames from 'classnames';
 import resumePDF from '@/assets/Resume.pdf';
 
 
-const projectTabs = ['Code Samples', 'Digital Design', 'Animations', 'Artworks'];
+const projectTabs = ['code samples', 'digital design', 'animations', 'artworks'];
 
 export default function Header({ page } : { page: PageTab }) {
   const navigate = useNavigate();
-  const [isDropdownVisible, setDropdownVisible] = useState(false);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const getClassName = (currentPage: string, targetPage: string, dropdown: boolean | null = null): string => {
     return classNames({
       [styles.selectedTab]: currentPage === targetPage || (dropdown !== null && dropdown),
-      [styles.projectsSelected]: targetPage == PageTab.projects && isDropdownVisible,
+      [styles.projectsSelected]: targetPage == PageTab.projects && open,
     });
   };
 
-  const handleOpen = () => {
-    setDropdownVisible(true);
-  };
-  const handleClose = () => {
-    setDropdownVisible(false);
-  };
 
-  const DropDownMenu = styled(({ className, ...props }: TooltipProps) => (
-    <Tooltip {...props} 
-      classes={{ popper: className }} 
-      disableFocusListener
-      placement="bottom" 
-    />
-  ))(() => ({
-    [`& .${tooltipClasses.tooltip}`]: {
-      padding: '0px',
-      borderRadius: '5px',
-      overflow: 'hidden',
-    },
-  }));
+
+  // const DropDownMenu = styled(({ className, ...props }: TooltipProps) => (
+  //   <Tooltip {...props} 
+  //     classes={{ popper: className }} 
+  //     disableFocusListener
+  //     placement="bottom" 
+  //   />
+  // ))(() => ({
+  //   [`& .${tooltipClasses.tooltip}`]: {
+  //     padding: '0px',
+  //     borderRadius: '5px',
+  //     overflow: 'hidden',
+  //   },
+  // }));
 
   return (
     <Box className={styles.headerContainer}>
@@ -52,31 +53,37 @@ export default function Header({ page } : { page: PageTab }) {
         >
           Home
         </Button>
-        <DropDownMenu
-          title={( 
-            <Box className={styles.menuItem}>
-              {projectTabs.map((item) => (
-                <MenuItem 
-                  key={item}
-                  onClick={() => navigate(`/projects/${item.toLowerCase().replace(/\s+/g, '')}`)}
-                >
-                  {item}
-                </MenuItem>
-              ))}
-            </Box>
-          )}
-          open={isDropdownVisible}
-          onOpen={handleOpen}
-          onClose={handleClose}
+        <Button
+          className={getClassName(page, PageTab.projects)}
+          onClick={handleClick}
+          disableRipple
         >
-          <Button
-            className={getClassName(page, PageTab.projects)}
-            onClick={handleOpen} 
-            disableRipple
-          >
-             Projects
-          </Button>
-        </DropDownMenu>
+          Projects
+        </Button>
+        <Menu
+          className={styles.menuContainer}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
+        >
+          {projectTabs.map((item) => (
+            <MenuItem
+              key={item}
+              onClick={() => navigate(`/projects/${item.replace(/\s+/g, '')}`)}
+              className={styles.menuItem}
+            >
+              {item}
+            </MenuItem>
+          ))}
+        </Menu>
         <Button 
           className={getClassName(page, PageTab.about)}
           onClick={() => { navigate('/about') }}
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { CssBaseline, Box } from '@mui/material';
 import Header from './components/header/Header.tsx'
 import Footer from './components/footer/Footer.tsx'
